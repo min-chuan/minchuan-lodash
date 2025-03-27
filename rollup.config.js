c9883f9ec7c837703412c7891441278b7591a9a7
minchuan <15868163725@163.com>
2025-03-27 21:00:54 +0800

    fix: export using es module

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,10 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'es',
-    },
-  ],
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+  },
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
