8ae3a70d2d49b680211b7af6870d02ebba365ec8
minchuan <15868163725@163.com>
2025-04-08 18:12:35 +0800

    fix: 添加base路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/minchuan-lodash/',
   title: 'minchuan-lodash',
   description: 'A lightweight lodash alternative',
   themeConfig: {
